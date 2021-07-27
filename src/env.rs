95c5ef7472a14db19bfe5d9105ea3669ee9cbc17
rprtr258 <poletansky@gmail.com>
2021-07-27 22:17:26 +0300

    Add arithmetic ops

--- src/env.rs
+++ src/env.rs
@@ -24,12 +24,47 @@ impl Env {
 
     pub fn new_repl() -> Env {
         let env = Env::new(None);
+        #[macro_export]
+        macro_rules! set_int_bin_op {
+            ($name:expr, $init:expr, $f:expr) => {
+                env.set(
+                    Atom::Symbol($name.to_string()),
+                    Atom::Func(|vals| vals.iter().fold(Ok(Atom::Int($init)), |a: LishRet, b: &Atom| match (a, b) {
+                        (Ok(Atom::Int(ai)), Atom::Int(bi)) => Ok(Atom::Int($f(ai, bi))),
+                        _ => error_string(format!("Can't eval ({} {:?})", $name, vals)),
+                    }), Rc::new(Atom::Nil))).unwrap();
+            };
+            ($name:expr, $f:expr) => {
+                env.set(
+                    Atom::Symbol($name.to_string()),
+                    Atom::Func(|vals| {
+                        let init = vals[0].clone();
+                        vals.iter().skip(1).fold(Ok(init), |a: LishRet, b: &Atom| match (a, b) {
+                        (Ok(Atom::Int(ai)), Atom::Int(bi)) => Ok(Atom::Int($f(ai, bi))),
+                        _ => error_string(format!("Can't eval ({} {:?})", $name, vals)),
+                    })}, Rc::new(Atom::Nil))).unwrap();
+            }
+        }
+        set_int_bin_op!("+", 0, |x, y| x + y);
+        set_int_bin_op!("*", 1, |x, y| x * y);
+        set_int_bin_op!("/", |x, y| x / y);
+        set_int_bin_op!("-", |x, y| x - y);
         env.set(
-            Atom::Symbol("+".to_string()),
-            Atom::Func(|vals| vals.iter().fold(Ok(Atom::Int(0)), |a: LishRet, b: &Atom| match (a, b) {
-                (Ok(Atom::Int(ai)), Atom::Int(bi)) => Ok(Atom::Int(ai + bi)),
-                _ => error_string(format!("Can't eval (+ {:?})", vals)),
-            }), Rc::new(Atom::Nil))).unwrap();
+            Atom::Symbol("-".to_string()),
+            Atom::Func(|vals| match vals.len() {
+                0 => error("Can't evaluate (-)"),
+                1 => match vals[0] {
+                    Atom::Int(x) => Ok(Atom::Int(-x)),
+                    _ => error_string(format!("Can't negate {:?}", vals[0])),
+                }
+                _ => {
+                    let init = vals[0].clone();
+                    vals.iter().skip(1).fold(Ok(init), |a: LishRet, b: &Atom| match (a, b) {
+                        (Ok(Atom::Int(ai)), Atom::Int(bi)) => Ok(Atom::Int(ai - bi)),
+                        _ => error_string(format!("Can't eval ({} {:?})", "-", vals)),
+                    })
+                }
+            }, Rc::new(Atom::Nil))).unwrap();
         env
     }
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,28 +4,43 @@ pub mod reader;
 mod printer;
 
 use crate::{
-    types::{Atom, LishRet, error_string},
+    types::{Atom, LishRet/*, error*/, error_string},
     env::{Env},
     reader::{read},
     printer::{print},
 };
 
 /*
-symbol "def!":
-    call the set method of the current environment (second parameter of EVAL called env) using the unevaluated first parameter (second list element) as the symbol key and the evaluated second parameter as the value.
 symbol "let*":
     create a new environment using the current environment as the outer value and then use the first parameter as a list of new bindings in the "let*" environment. Take the second element of the binding list, call EVAL using the new "let*" environment as the evaluation environment, then call set on the "let*" environment using the first binding list element as the key and the evaluated second element as the value. This is repeated for each odd/even pair in the binding list. Note in particular, the bindings earlier in the list can be referred to by later bindings. Finally, the second parameter (third element) of the original let* form is evaluated using the new "let*" environment and the result is returned as the result of the let* (the new let environment is discarded upon completion). */
 pub fn eval(cmd: &Atom, env: &Env) -> LishRet {
     match cmd {
-        Atom::Symbol(var) => {
-            env.get(var)
-        }
+        Atom::Symbol(var) => env.get(var),
         Atom::List(items, _) => {
             match &items[0] {
                 Atom::Symbol(sym) => {
                     match &sym[..] {
                         "set" => {
-                            env.set(items[1].clone(), items[2].clone())
+                            assert_eq!(items.len(), 3);
+                            let value: Atom = eval(&items[2], env)?;
+                            env.set(items[1].clone(), value)
+                        }
+                        "let" => {
+                            let bindings = match &items[1] {
+                                Atom::List(xs, _) => xs,
+                                _ => return error_string(format!("Let bindings is not a list, but a {:?}", items[1])),
+                            };
+                            assert_eq!(bindings.len() % 2, 0);
+                            let mut i = 0;
+                            let let_env = Env::new(Some(env.clone()));
+                            while i < bindings.len() {
+                                let var_name = bindings[i].clone();
+                                let var_value = eval(&bindings[i + 1], &let_env)?;
+                                let_env.set(var_name, var_value)?;
+                                i += 2;
+                            }
+                            let body = &items[2];
+                            eval(body, &let_env)
                         }
                         fun_name => {
                             let fun = env.get(fun_name)?;
@@ -53,6 +68,7 @@ pub fn rep(input: &String, env: &Env) {
 }
 
 // TODO: add tests from history.txt
+// TODO: do fucking macro
 #[cfg(test)]
 mod eval_tests {
     use crate::{
@@ -64,10 +80,25 @@ mod eval_tests {
     #[test]
     fn set() {
         let repl_env = Env::new_repl();
-        assert_eq!(eval(&list(vec![Symbol("set".to_string()), Symbol("a".to_string()), Int(2)]), &repl_env).unwrap(), Int(2));
-        assert_eq!(eval(&list(vec![Symbol("+".to_string()), Symbol("a".to_string()), Int(3)]), &repl_env).unwrap(), Int(5));
-        assert_eq!(eval(&list(vec![Symbol("set".to_string()), Symbol("b".to_string()), Int(3)]), &repl_env).unwrap(), Int(3));
-        assert_eq!(eval(&list(vec![Symbol("+".to_string()), Symbol("a".to_string()), Symbol("b".to_string())]), &repl_env).unwrap(), Int(5));
+        assert_eq!(eval(
+            &list(vec![Symbol("set".to_string()), Symbol("a".to_string()), Int(2)]),
+            &repl_env).unwrap(), Int(2));
+        assert_eq!(eval(
+            &list(vec![Symbol("+".to_string()), Symbol("a".to_string()), Int(3)]),
+            &repl_env).unwrap(), Int(5));
+        assert_eq!(eval(
+            &list(vec![Symbol("set".to_string()), Symbol("b".to_string()), Int(3)]),
+            &repl_env).unwrap(), Int(3));
+        assert_eq!(eval(
+            &list(vec![Symbol("+".to_string()), Symbol("a".to_string()), Symbol("b".to_string())]),
+            &repl_env).unwrap(), Int(5));
+        assert_eq!(eval(
+            &list(vec![Symbol("set".to_string()), Symbol("c".to_string()),
+                list(vec![Symbol("+".to_string()), Int(1), Int(2)])]),
+            &repl_env).unwrap(), Int(3));
+        assert_eq!(eval(
+            &list(vec![Symbol("+".to_string()), Symbol("c".to_string()), Int(1)]),
+            &repl_env).unwrap(), Int(4));
     }
 
     #[test]
@@ -79,9 +110,99 @@ mod eval_tests {
     }
 
     #[test]
+    fn multiply() {
+        let repl_env = Env::new_repl();
+        // (*)
+        assert_eq!(eval(
+            &list(vec![Symbol("*".to_string()), Int(1)]),
+            &repl_env).unwrap(), Int(1));
+        // (* 2)
+        assert_eq!(eval(
+            &list(vec![Symbol("*".to_string()), Int(2)]),
+            &repl_env).unwrap(), Int(2));
+        // (* 1 2 3)
+        assert_eq!(eval(
+            &list(vec![Symbol("*".to_string()), Int(1), Int(2), Int(3)]),
+            &repl_env).unwrap(), Int(6));
+    }
+
+    #[test]
+    fn divide() {
+        let repl_env = Env::new_repl();
+        // (/ 1)
+        assert_eq!(eval(
+            &list(vec![Symbol("/".to_string()), Int(1)]),
+            &repl_env).unwrap(), Int(1));
+        // (/ 5 2)
+        assert_eq!(eval(
+            &list(vec![Symbol("/".to_string()), Int(5), Int(2)]),
+            &repl_env).unwrap(), Int(2));
+        // (/ 22 3 2)
+        assert_eq!(eval(
+            &list(vec![Symbol("/".to_string()), Int(22), Int(3), Int(2)]),
+            &repl_env).unwrap(), Int(3));
+    }
+
+    #[test]
+    fn minus() {
+        let repl_env = Env::new_repl();
+        // (- 1)
+        assert_eq!(eval(
+            &list(vec![Symbol("-".to_string()), Int(1)]),
+            &repl_env).unwrap(), Int(-1));
+        // (- 1 2 3)
+        assert_eq!(eval(
+            &list(vec![Symbol("-".to_string()), Int(1), Int(2), Int(3)]),
+            &repl_env).unwrap(), Int(-4));
+    }
+
+    #[test]
     fn plus() {
         let repl_env = Env::new_repl();
-        assert_eq!(eval(&list(vec![Symbol("+".to_string()), Int(1), Int(2), Int(3)]), &repl_env).unwrap(), Int(6));
-        assert_eq!(eval(&list(vec![Symbol("+".to_string()), Int(1), Int(2), list(vec![Symbol("+".to_string()), Int(1), Int(2)])]), &repl_env).unwrap(), Int(6));
+        // (+)
+        assert_eq!(eval(
+            &list(vec![Symbol("+".to_string())]),
+            &repl_env).unwrap(), Int(0));
+        // (+ 1)
+        assert_eq!(eval(
+            &list(vec![Symbol("+".to_string()), Int(1)]),
+            &repl_env).unwrap(), Int(1));
+        // (+ 1 2 3)
+        assert_eq!(eval(
+            &list(vec![Symbol("+".to_string()), Int(1), Int(2), Int(3)]),
+            &repl_env).unwrap(), Int(6));
+        // (+ 1 2 (+ 1 2))
+        assert_eq!(eval(
+            &list(vec![Symbol("+".to_string()), Int(1), Int(2), list(vec![Symbol("+".to_string()), Int(1), Int(2)])]),
+            &repl_env).unwrap(), Int(6));
+    }
+
+    #[test]
+    fn let_statement() {
+        let repl_env = Env::new_repl();
+        // (set a 2)
+        assert_eq!(eval(
+            &list(vec![Symbol("set".to_string()), Symbol("a".to_string()), Int(2)]),
+            &repl_env).unwrap(), Int(2));
+        // (let (a 1) a)
+        assert_eq!(eval(
+            &list(vec![Symbol("let".to_string()), 
+                list(vec![Symbol("a".to_string()), Int(1)]),
+                Symbol("a".to_string())]),
+            &repl_env).unwrap(), Int(1));
+        // a
+        assert_eq!(eval(&Symbol("a".to_string()), &repl_env).unwrap(), Int(2));
+        // (let (a 1 b 2) (+ a b))
+        assert_eq!(eval(
+            &list(vec![Symbol("let".to_string()), 
+                list(vec![Symbol("a".to_string()), Int(1), Symbol("b".to_string()), Int(2)]),
+                list(vec![Symbol("+".to_string()), Symbol("a".to_string()), Symbol("b".to_string())])]),
+            &repl_env).unwrap(), Int(3));
+        // (let (a 1 b a) b)
+        assert_eq!(eval(
+            &list(vec![Symbol("let".to_string()), 
+                list(vec![Symbol("a".to_string()), Int(1), Symbol("b".to_string()), Symbol("a".to_string())]),
+                Symbol("b".to_string())]),
+            &repl_env).unwrap(), Int(1));
     }
 }
