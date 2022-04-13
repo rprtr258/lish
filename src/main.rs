28bea2ef59e27087dd3090397682b3759a405320
Poletansky Viktor <poletansky@gmail.com>
2022-04-14 01:39:10 +0300

    comment_helper

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,12 @@ use std::env::args;
 use rustyline::{
     error::ReadlineError,
     Editor,
+    //Helper,
+    //Context,
+    //validate::Validator,
+    //highlight::Highlighter,
+    //hint::Hinter,
+    //completion::Completer,
 };
 use lish::{
     env::Env,
@@ -11,8 +17,39 @@ use lish::{
 
 fn main() {
     const HISTORY_FILE: &str = ".lish_history";
+    // struct LishHelper {}
+    // impl Validator for LishHelper {}
+    // impl Highlighter for LishHelper {}
+    // impl Hinter for LishHelper {
+    //     type Hint = String;
+
+    //     fn hint(&self, line: &str, pos: usize, ctx: &Context<'_>) -> Option<Self::Hint> {
+    //         let history = ctx.history();
+    //         let history_index = ctx.history_index();
+    //         //println!("history: {:?}, index: {}", history.iter().collect::<Vec<&String>>(), history_index);
+    //         //println!("{} at {}", line, pos);
+    //         Some(history
+    //             .get(history_index)
+    //             .map(|s| s[line.len()..].to_owned())
+    //             .unwrap_or("".to_owned())
+    //         )
+    //     }
+    // }
+    // impl Completer for LishHelper {
+    //     type Candidate = String;
+
+    //     fn complete(&self, line: &str, pos: usize, ctx: &Context<'_>) -> rustyline::Result<(usize, Vec<Self::Candidate>)> {
+    //         let _ = (line, pos, ctx);
+    //         Ok((0, vec!["pokus".to_owned(), "fokus".to_owned()]))
+    //     }
+    // }
+    // impl Helper for LishHelper {}
+    // let mut editor = Editor::<LishHelper>::new();
+    // let helper = LishHelper {};
+    // editor.set_helper(Some(helper));
     let mut editor = Editor::<()>::new();
     if editor.load_history(HISTORY_FILE).is_err() {
+        // TODO: do nothing
         println!("No previous history.");
     }
     let repl_env = Env::new_repl();
