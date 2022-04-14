32b7964fb5ed429b2e427de5de641515d73bf6c4
Poletansky Viktor <poletansky@gmail.com>
2022-04-14 22:11:34 +0300

    tests

--- src/printer.rs
+++ src/printer.rs
@@ -46,7 +46,6 @@ pub fn print_debug(val: &Atom) -> String {
 #[cfg(test)]
 mod printer_tests {
     use std::rc::Rc;
-
     use crate::{
         form,
         types::Atom,
@@ -71,34 +70,28 @@ mod printer_tests {
         }
     }
 
+    fn make_hashmap() -> Atom {
+        let mut hashmap = fnv::FnvHashMap::default();
+        hashmap.insert("a".to_owned(), Atom::Int(1));
+        hashmap.insert("b".to_owned(), Atom::String("2".to_owned()));
+        Atom::Hash(std::rc::Rc::new(hashmap))
+    }
+
     test_print!(print_true, true, "true");
     test_print!(print_false, false, "false");
     test_print!(print_float, 3.14, "3.14");
     test_print!(print_int, 92, "92");
-    test_print!(print_empty_list, form![], "()");
+    test_print!(print_empty_list, Atom::Nil, "()");
     test_print!(print_list, form![1, 2], "(1 2)");
     test_print!(print_symbol, Atom::symbol("abc"), "abc");
+    test_print!(test_print_nice, Atom::from("\n"), "\n");
+    test_print!(test_print_dict, make_hashmap(), r#"{"a" 1 "b" "2"}"#);
+
     test_print_debug!(print_func, Atom::Func(|x| x[0].clone(), Rc::new(Atom::Nil)), "#fn");
     test_print_debug!(print_nil, Atom::Nil, "()");
     test_print_debug!(print_string, "abc", r#""abc""#);
     test_print_debug!(print_string_with_slash, r"\", r#""\\""#);
     test_print_debug!(print_string_with_2slashes, r"\\", r#""\\\\""#);
     test_print_debug!(print_string_with_newline, "\n", r#""\n""#);
-
-    #[test]
-    fn test_print_dict() {
-        let hashmap = Atom::Hash(std::rc::Rc::new({
-            let mut hashmap = fnv::FnvHashMap::default();
-            hashmap.insert("a".to_owned(), Atom::Int(1));
-            hashmap.insert("b".to_owned(), Atom::String("2".to_owned()));
-            hashmap
-        }));
-        assert_eq!(print(&hashmap), r#"{"a" 1 "b" "2"}"#);
-        assert_eq!(print_debug(&hashmap), r#"{"a" 1 "b" "2"}"#);
-    }
-
-    #[test]
-    fn test_print_nice() {
-        assert_eq!(print(&Atom::from("\n")), "\n")
-    }
+    test_print_debug!(test_print_debug_dict, make_hashmap(), r#"{"a" 1 "b" "2"}"#);
 }
